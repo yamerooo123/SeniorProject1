18622f3a4def5797ffbae6d292164be95639d356
suphwaith phusanbai <suprawitch123@gmail.com>
2023-06-22 15:49:18 +0700

    update

--- web_project/settings.py
+++ web_project/settings.py
@@ -52,8 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
+    
 
 ]
 
