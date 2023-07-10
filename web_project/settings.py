4626653ef1198bf6d737130f471f91c2d30119d3
suphwaith phusanbai <suprawitch123@gmail.com>
2023-07-10 20:01:49 +0700

    Update database configuration for ClearDB

--- web_project/settings.py
+++ web_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os.path
 import os
 
@@ -81,16 +82,8 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'web_project',
-        'USER': 'root',
-        'PASSWORD': '19752518M',
-        'HOST': '/var/run/mysqld/mysqld.sock',
-        'PORT': '3306',
-    }
+    'default': dj_database_url.config(),
 }
-
 
 
 
