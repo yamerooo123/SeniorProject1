d63bb6db07098f6b5e9ca5c0fbe6355640d8c31d
suphwaith phusanbai <suprawitch123@gmail.com>
2023-07-21 17:26:45 +0700

    update

--- web_project/settings.py
+++ web_project/settings.py
@@ -138,8 +138,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/Static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'Static'),
+)
 STATIC_ROOT = 'C:/Users/monaliza/Desktop/SP1/staticfiles'
 
 # Default primary key field type
